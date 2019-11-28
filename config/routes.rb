eb93a455249663ba73a989ee039dd514c61fa173
akinori0714 <akinori_ichimura@yahoo.co.jp>
2019-11-28 17:30:57 +0900

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'messages/index'
+
+  devise_for :users
+  root 'messages#index'
+  resources :users, only: [:edit, :update]
 end
