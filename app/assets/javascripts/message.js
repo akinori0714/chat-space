1aa5df5b7b74b894a55ea92ed1515af2e5c04b1a
akinori0714 <akinori_ichimura@yahoo.co.jp>
2019-12-05 17:21:10 +0900

    修正しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,7 @@
 $(function(){ 
   function buildHTML(message){
-   if ( message.image ) {
+    var addImage = message.image? `<img class = "image_size", src="${message.image}">` : ''
+
      var html =
       `<div class="message" data-message-id=${message.id}>
          <div class="upper-message">
@@ -16,29 +17,12 @@ $(function(){
              ${message.content}
            </p>
          </div>
-         <img src=${message.image} >
+         <img src=${message.image}>
+           ${addImage}
+
        </div>`
      return html;
-   } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.date}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
- }
+  }
 $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
